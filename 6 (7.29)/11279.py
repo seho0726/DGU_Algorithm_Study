d9dd2ee555f1bf76d21ad50e06ffcb4ea7be3de7
seho0726 <150892874+seho0726@users.noreply.github.com>
2024-07-29 19:41:03 +0900

    11279 제출

---
+++ 6 (7.29)/11279.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+
+n = int(input())
+heap = []
+
+for i in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if heap:
+            print((-1)*heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap,(-1)*x)
